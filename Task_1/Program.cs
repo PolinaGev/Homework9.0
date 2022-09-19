d2d6a9367e034917624296ee6e9be5c4538d8dc3
PolinaG <PolinaG@gmail.com>
2022-09-19 22:07:23 +0300

    new commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿string GetNaturalsBetweenWithRecursion(int m, int n)
+{
+  if (m == n) return Convert.ToString(n);
+  return m + ", " + GetNaturalsBetweenWithRecursion(m + 1, n);
+}
+System.Console.WriteLine(GetNaturalsBetweenWithRecursion(1,5));
---
+++ Task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿int AkkermanRec(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AkkermanRec(m - 1, 1);
+    return AkkermanRec(m-1, AkkermanRec(m, n-1));
+}
+System.Console.WriteLine(AkkermanRec(2,3));
